1516ad58eb75677fcb5af3fb429e62a7e0c868d1
Atla33 <dantasanderson57@hotmail.com>
2023-09-04 19:28:53 -0300

    auth: login agora é público

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,19 +6,22 @@ import {
     HttpStatus,
     Post,
     Request,
-    UseGuards
+    UseGuards,
   } from '@nestjs/common';
   import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
+import { IsPublic } from './decorators/is-public.decorator';
   
   @Controller()
   export class AuthController {
     constructor(private readonly authService: AuthService) {}
   
+    @IsPublic()
     @Post('login')
     @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
-    login(@Request() req){
+    login(@Request() req: AuthRequest){
       return this.authService.login(req.user);
     }
   }
