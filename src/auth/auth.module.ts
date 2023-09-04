238a4d80bcf9db7e9e37ea153a0ac6a285ae8627
Atla33 <dantasanderson57@hotmail.com>
2023-09-04 20:00:12 -0300

    auth: implementação do login validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { LocalStrategy } from './strategies/local.strategy';
-import { UserModule } from 'src/user/user.module';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { LoginValidationMiddleware } from './middlewares/login-validation.middleware';
 
 
 @Module({
   imports: [
     UserModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
@@ -20,4 +23,8 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   exports: [AuthService],
 
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
